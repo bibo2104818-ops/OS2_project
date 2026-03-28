09c40bf8e6c2d760116bc7cf73c527e79f8fce2b
imadtaibi573-design <imadtaibi573@gmail.com>
2026-03-28 12:39:53 +0100

    Update main.sh

--- scripts/main.sh
+++ scripts/main.sh
@@ -9,125 +9,27 @@ NC='\033[0m'
 WHITE='\033[0;37m'
 
 
+program(){ 
 
+ loop=true
 
-interface(){
-
-
-                echo -e "${CYAN}"
-   echo " ██████╗ ███████╗    ████████╗ ██████╗  ██████╗ ██╗     ███████╗"
-   echo "██╔═══██╗██╔════╝    ╚══██╔══╝██╔═══██╗██╔═══██╗██║     ██╔════╝"
-   echo "██║   ██║███████╗       ██║   ██║   ██║██║   ██║██║     ███████╗"
-   echo "██║   ██║╚════██║       ██║   ██║   ██║██║   ██║██║     ╚════██║"
-   echo "╚██████╔╝███████║       ██║   ╚██████╔╝╚██████╔╝███████╗███████║"
-   echo " ╚═════╝ ╚══════╝       ╚═╝    ╚═════╝  ╚═════╝ ╚══════╝╚══════╝"
-   echo -e "${NC}"
-   echo -e "${YELLOW}/-------------GOODBYE------------\ ${NC}"
-
-
-}
-
-
-
-
-echo -e "${WHITE}===============================================${RESET}"
-
-echo -e "\e[30m"
-echo "  ██████  ███████     ██████  ██████   ██████       ██ ███████  ██████ ████████ "
-echo " ██    ██ ██          ██   ██ ██   ██ ██    ██      ██ ██      ██         ██    "
-echo " ██    ██ ███████     ██████  ██████  ██    ██      ██ █████   ██         ██    "
-echo " ██    ██      ██     ██      ██   ██ ██    ██ ██   ██ ██      ██         ██    "
-echo "  ██████  ███████     ██      ██   ██  ██████   █████  ███████  ██████    ██    "
-echo "                                                                                "
-echo "                     ██   ██ ████████  ██████   ██████  ██      ███████         "
-echo "                     ██   ██    ██    ██    ██ ██    ██ ██      ██              "
-echo "                     ███████    ██    ██    ██ ██    ██ ██      ███████         "
-echo "                     ██   ██    ██    ██    ██ ██    ██ ██           ██         "
-echo "                     ██   ██    ██     ██████   ██████  ███████ ███████         "
-echo -e "\e[0m"
-echo -e "${WHITE}=================================================${RESET}"
-
-
-show_project_info() {
-    echo -e "${CYAN}======================================${NC}"
-    echo -e "${GREEN}        WELCOME $(whoami) TO MY TOOLKIT         ${NC}"
-    echo -e "${CYAN}======================================${NC}"
-    echo -e "${YELLOW}This project is a modular Bash toolkit that can:"
-    echo "- Show hardware info"
-    echo "- Install software or dependencies"
-    echo "- Manage remote access and SSH"
-    echo "- Run custom scripts"
-    echo "- Send emails safely"
-    echo "- Compare reports"
-    echo "- Manage cron jobs"
-    echo -e "${CYAN}Developed by:${NC}"
-    echo -e "${GREEN}Lahlouh AbdElJalil${NC} and ${GREEN}Imad Taibi${NC}, students at the National School of Cybersecurity"
-
-    read -p "Press Enter to continue to the main menu..."
-}
-
-
-
-
-#---small information about the user that use this script-----
-
-echo -e "${CYAN}========== User Information ==========${NC}"
-
-echo -e "${GREEN}Username: ${NC}$(whoami)"
-
-echo -e "${GREEN}UID: ${NC}$(id -u)  GID: ${NC}$(id -g)"
-
-echo -e "${GREEN}Groups: ${NC}$(id -Gn)"
-
-echo -e "${GREEN}Hostname: ${NC}$(hostname)"
-
-if [ -f /etc/os-release ]; then
-    OS_NAME=$(grep '^PRETTY_NAME=' /etc/os-release | cut -d= -f2 | tr -d '"')
-    echo -e "${GREEN}Operating System: ${NC}${OS_NAME}"
-fi
-
-echo -e "${GREEN}Home Directory: ${NC}$HOME"
-
-echo -e "${GREEN}Shell: ${NC}$SHELL"
-
-echo -e "${CYAN}=====================================${NC}"
-
-
-echo -e "${CYAN}Do you want to see project information or go directly to the program?${NC}"
-echo -e "${GREEN}1) Show project information${NC}"
-echo -e "${GREEN}2) Skip to main menu${NC}"
-
-read -p "Choose an option: " pre_choice
-case $pre_choice in
-    1)
-        show_project_info
-        ;;
-    2)
-        echo -e "${YELLOW}Skipping to main menu...${NC}"
-        ;;
-    *)
-        echo -e "${RED}Invalid option, continuing to main menu...${NC}"
-        ;;
-esac
-
-loop=true
-
-while $loop
-do
+ while $loop
+ do
 
     echo -e "${BLUE}Choose one option${NC}"
-    echo -e "${WHITE} __________________________${NC}"
-    echo -e "${WHITE}| ${GREEN}1) Hardware Info${NC}         |"
-    echo -e "${WHITE}| ${GREEN}2) Install${NC}               |"
-    echo -e "${WHITE}| ${GREEN}3) Remote Access${NC}         |"
-    echo -e "${WHITE}| ${GREEN}4) Run Script${NC}            |"
-    echo -e "${WHITE}| ${GREEN}5) Send Email${NC}            |"
-    echo -e "${WHITE}| ${GREEN}6) Setup SSH${NC}             |"
-    echo -e "${WHITE}| ${GREEN}7) Software Info${NC}         |"
-    echo -e "${WHITE}| ${GREEN}8) cron job manager${NC}      |" 
-    echo -e "${WHITE}| ${GREEN}9) comparing two reports${NC} |"
-    echo -e "${WHITE}| ${RED}0) Exit${NC}                  |"
-    echo -e "${WHITE}|__________________________|${NC}"
+    echo -e "${WHITE} _____________________________${NC}"
+    echo -e "${WHITE}| ${GREEN}1) Hardware Info${NC}            |"
+    echo -e "${WHITE}| ${GREEN}2) Install${NC}                  |"
+    echo -e "${WHITE}| ${GREEN}3) Remote Access${NC}            |"
+    echo -e "${WHITE}| ${GREEN}4) Run Script${NC}               |"
+    echo -e "${WHITE}| ${GREEN}5) Send Email${NC}               |"
+    echo -e "${WHITE}| ${GREEN}6) Setup SSH${NC}                |"
+    echo -e "${WHITE}| ${GREEN}7) Software Info${NC}            |"
+    echo -e "${WHITE}| ${GREEN}8) cron job manager${NC}         |" 
+    echo -e "${WHITE}| ${GREEN}9) comparing two reports${NC}    |"
+    echo -e "${WHITE}| ${RED}10) Exit${NC}                    |"
+    echo -e "${WHITE}| ${BLUE}0) Go back to the main menu${NC} |"
+    echo -e "${WHITE}|_____________________________|${NC}"
 
     echo -e "${CYAN}==============================${NC}"
     read -p "Choose an option: " choice
@@ -168,16 +70,23 @@ do
 
         9)
            echo -e "Running Script....${NC}"
+ 
            bash comparing_report.sh
         ;;
 
-        0)
+        10)
             echo -e "${RED}Exiting...${NC}"
 
                 interface
 
             exit 0
             ;;
+
+        0)
+        clear
+         main_menu
+         ;;
+
         *)
             echo -e "${RED}Invalid option!${NC}"
             ;;
@@ -208,4 +117,132 @@ do
 
     esac
 
-done
+ done
+
+}
+
+
+show_project_info(){
+    echo -e "${CYAN}======================================${NC}"
+    echo -e "${GREEN}        WELCOME $(whoami) TO MY TOOLKIT         ${NC}"
+    echo -e "${CYAN}======================================${NC}"
+    echo -e "${YELLOW}This project is a modular Bash toolkit that can:"
+    echo "- Show hardware info"
+    echo "- Install software or dependencies"
+    echo "- Manage remote access and SSH"
+    echo "- Run custom scripts"
+    echo "- Send emails safely"
+    echo "- Compare reports"
+    echo "- Manage cron jobs"
+    echo -e "${CYAN}Developed by:${NC}"
+    echo -e "${GREEN}Lahlouh AbdElJalil${NC} and ${GREEN}Imad Taibi${NC}, students at the National School of Cybersecurity"
+    echo -e "${BLUE}Check out our project page: https://github.com/bibo2104818-ops/OS2_project"
+    echo -e "${YELLOW} Do you want to open link"
+    echo    "1) YES"
+    echo -e "2) NO${NC}"
+    read ch1
+    case $ch1 in 
+    
+    1)
+      xdg-open https://github.com/bibo2104818-ops/OS2_project
+      ;;
+    2)
+    ;;
+    *)
+    echo -e "${RED}Invalid option${NC}"
+    ;; 
+ esac
+
+    read -p "Press Enter to continue to the main menu..."
+    program
+    clear
+    
+}
+
+interface(){
+
+                echo -e "${CYAN}"
+   echo " ██████╗ ███████╗    ████████╗ ██████╗  ██████╗ ██╗     ███████╗"
+   echo "██╔═══██╗██╔════╝    ╚══██╔══╝██╔═══██╗██╔═══██╗██║     ██╔════╝"
+   echo "██║   ██║███████╗       ██║   ██║   ██║██║   ██║██║     ███████╗"
+   echo "██║   ██║╚════██║       ██║   ██║   ██║██║   ██║██║     ╚════██║"
+   echo "╚██████╔╝███████║       ██║   ╚██████╔╝╚██████╔╝███████╗███████║"
+   echo " ╚═════╝ ╚══════╝       ╚═╝    ╚═════╝  ╚═════╝ ╚══════╝╚══════╝"
+   echo -e "${NC}"
+   echo -e "${YELLOW}/-------------GOODBYE------------\ ${NC}"
+
+}
+
+main_menu(){ 
+
+ echo -e "${WHITE}===============================================${RESET}"
+
+ echo -e "\e[30m"
+ echo "  ██████  ███████     ██████  ██████   ██████       ██ ███████  ██████ ████████ "
+ echo " ██    ██ ██          ██   ██ ██   ██ ██    ██      ██ ██      ██         ██    "
+ echo " ██    ██ ███████     ██████  ██████  ██    ██      ██ █████   ██         ██    "
+ echo " ██    ██      ██     ██      ██   ██ ██    ██ ██   ██ ██      ██         ██    "
+ echo "  ██████  ███████     ██      ██   ██  ██████   █████  ███████  ██████    ██    "
+ echo "                                                                                "
+ echo "                     ██   ██ ████████  ██████   ██████  ██      ███████         "
+ echo "                     ██   ██    ██    ██    ██ ██    ██ ██      ██              "
+ echo "                     ███████    ██    ██    ██ ██    ██ ██      ███████         "
+ echo "                     ██   ██    ██    ██    ██ ██    ██ ██           ██         "
+ echo "                     ██   ██    ██     ██████   ██████  ███████ ███████         "
+ echo -e "\e[0m"
+ echo -e "${WHITE}=================================================${RESET}"
+
+
+
+
+
+
+
+ #---small information about the user that use this script-----
+
+ echo -e "${CYAN}========== User Information ==========${NC}"
+
+ echo -e "${GREEN}Username: ${NC}$(whoami)"
+
+ echo -e "${GREEN}UID: ${NC}$(id -u)  GID: ${NC}$(id -g)"
+
+ echo -e "${GREEN}Groups: ${NC}$(id -Gn)" 
+
+ echo -e "${GREEN}Hostname: ${NC}$(hostname)"
+
+ if [ -f /etc/os-release ]; then
+    OS_NAME=$(grep '^PRETTY_NAME=' /etc/os-release | cut -d= -f2 | tr -d '"')
+    echo -e "${GREEN}Operating System: ${NC}${OS_NAME}"
+ fi
+
+ echo -e "${GREEN}Home Directory: ${NC}$HOME"
+
+ echo -e "${GREEN}Shell: ${NC}$SHELL"
+
+ echo -e "${CYAN}=====================================${NC}"
+
+
+
+ echo -e "${CYAN}Do you want to see project information or go directly to the program?${NC}"
+ echo -e "${GREEN}1) Show project information${NC}"
+ echo -e "${GREEN}2) Skip to main menu${NC}"
+
+ read -p "Choose an option: " pre_choice
+ case $pre_choice in 
+    1)
+        show_project_info
+        ;;
+    2)
+        echo -e "${YELLOW}Skipping to main menu...${NC}"
+        program
+        ;;
+    *)
+        echo -e "${RED}Invalid option, continuing to main menu...${NC}"
+        program
+        ;;
+ esac
+
+}
+
+
+main_menu
